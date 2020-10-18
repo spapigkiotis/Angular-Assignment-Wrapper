057981e9167dd9e66b9fa6ba814e6ecd04f25efc
spapigkiotis <stergiospapigkiotis@gmail.com>
2020-10-18 22:28:23 +0300

    Wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WrapperModule } from './wrapper/wrapper.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WrapperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/wrapper/wrapper.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { HeaderComponent } from './wrapper/header/header.component';
+import { NavComponent } from './wrapper/nav/nav.component';
+import { ContentComponent } from './wrapper/content/content.component';
+import { FooterComponent } from './wrapper/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [WrapperComponent, HeaderComponent, NavComponent, ContentComponent, FooterComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [WrapperComponent, HeaderComponent, NavComponent, ContentComponent, FooterComponent]
+})
+export class WrapperModule { }
